06f7a199eaa23f9a0e16dfd8395bfb2008507798
Yago Garcez <yago.garcez@outlook.com>
2023-09-26 21:07:45 -0300

    Backend project created.

---
+++ dscrudclient-backend/src/main/java/com/example/dscrudclientbackend/DscrudclientBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dscrudclientbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscrudclientBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscrudclientBackendApplication.class, args);
+	}
+
+}
---
+++ dscrudclient-backend/src/test/java/com/example/dscrudclientbackend/DscrudclientBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dscrudclientbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrudclientBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
